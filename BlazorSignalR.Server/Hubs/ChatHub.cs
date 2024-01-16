2dee25ceaeef2ec02d57dbbfecb29be017d536e1
Jericho <jerichoeugenio@simplifiedph.com>
2024-01-16 19:24:35 +0800

    with group

--- BlazorSignalR.Server/Hubs/ChatHub.cs
+++ BlazorSignalR.Server/Hubs/ChatHub.cs
@@ -4,9 +4,23 @@ namespace BlazorSignalR.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message) 
+        public async Task SendMessage(string message) 
         {
-            await Clients.All.SendAsync("ReceivedMessage", user, message);
+            await Clients.All.SendAsync("ReceivedMessage", message);
+        }
+        public Task AddGroup(string group)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public Task RemoveGroup(string groupName)
+        {
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task SendMessageToGroup(string groupName, string message)
+        {
+            await Clients.Group(groupName).SendAsync("ReceivedMessage", message);
         }
     }
 }
